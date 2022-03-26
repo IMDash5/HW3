4b71ce7afbf38af0599bbe67ab961fa182760c28
Ivan <mmavrin80@gmail.com>
2022-03-26 17:39:19 +0300

    ask3

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strNum = Console.ReadLine();
+    int Num = int.Parse(strNum);
+    return Num;
+}
+
+int N = Prompt("Введите число ");
+
+int i = 1;
+double res = 0;
+
+if (N > 0)
+{
+    while (i <= N)
+    {
+        res = Math.Pow(i, 3);
+        Console.Write($"{res} ");
+        i = i + 1;
+    }
+}
+else 
+{
+    while (i >= N)
+    {
+        res = Math.Pow(i, 3);
+        Console.Write($"{res} ");
+        i = i - 1;
+    }
+}
